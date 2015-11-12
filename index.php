9a3770e48a924d9803ae229ad60cdba732d713c8
tengshan2008 <tengshan2008@hotmail.com>
2015-11-12 18:10:17 +0800

    增加导航网格

--- index.php
+++ index.php
@@ -9,8 +9,34 @@
         <title></title>
     </head>
     <body>
-        Welcome to matrixgo
-	</br>
-	hello world!!!
-    </body>
+    <h1 align="center">导航网格</h1>
+
+
+    <table width="100%" border="1">
+        <tr>
+          <a href="web1/index.php"><td width="25%" align="center">智能饲养</td></a>
+          <td width="25%">&nbsp;</td>
+          <td width="25%">&nbsp;</td>
+          <td width="25%">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+    </table>
+</body>
 </html>
