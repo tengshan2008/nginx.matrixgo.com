852ff412ff37c1f2b0f735b5075bfbdd200b2bc9
tengshan2008 <tengshan2008@hotmail.com>
2015-11-12 13:53:50 +0800

    tian jia mo ban

---
+++ web1/index.php
@@ -0,0 +1,185 @@
+<?php
+
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="generator" content="9466 Design Engine 2.0.3" />
+    <title>首页</title>
+    <link rel="stylesheet" href="assets/index.css?1447307405" />
+    <script type="text/javascript" src="assets/index.js?1447307405"></script>
+    <!--[if IE 6]>
+    <script> try { document.execCommand('BackgroundImageCache', false, true); } catch(e) {} </script>
+    <![endif]-->
+  </head>
+  <body id="w-body-aaaa" data-type="body" class="p-166372 n-widget w-body">
+    <div id="w-area-aaab" data-type="area" class="p-166372 n-widget w-area">
+      <div id="w-area-aaac" data-type="area" class="p-166372 n-widget w-area">
+        <div id="w-columnbox-aaad" data-type="columnbox" class="n-widget w-columnbox">
+            <div class="w-col" style="width:25%;">
+            <div id="w-area-aaae" data-type="area" class="p-166372 n-widget w-area">
+              <div id="w-image-aaaf" data-type="image" class="n-widget w-image">
+                <img src="assets/97306da3e65992d7ef0d10925c9ba2b1.png" />
+              </div>
+            </div>
+            </div>
+            <div class="w-col" style="width:75%;">
+            <div id="w-area-aaag" data-type="area" class="p-166372 n-widget w-area">
+              <div id="w-menu-aaah" data-type="menu" class="n-widget w-menu t-menu-05 t-menu-05-39cb9b">
+              <div class="w-menu-container" data-menu-type="h-down"><ul class="w-menu-list"><li class="w-menu-item w-menu-item-first active"><a href="./index.html">首页</a></li><li class="w-menu-divider"><span class="w-menu-divider-item"></span></li><li class="w-menu-item"><a href="./intro.html">公司简介</a></li><li class="w-menu-divider"><span class="w-menu-divider-item"></span></li><li class="w-menu-item"><a href="./product.html">产品中心</a></li><li class="w-menu-divider"><span class="w-menu-divider-item"></span></li><li class="w-menu-item"><a href="./news.html">新闻动态</a></li><li class="w-menu-divider"><span class="w-menu-divider-item"></span></li><li class="w-menu-item"><a href="./document.html">产品知识</a></li><li class="w-menu-divider"><span class="w-menu-divider-item"></span></li><li class="w-menu-item w-menu-item-last"><a href="./about.html">联系我们</a></li></ul></div>
+              </div>
+            </div>
+            </div>
+        </div>
+        <div id="w-area-aaai" data-type="area" class="p-166372 n-widget w-area">
+          <div id="w-image-aaaj" data-type="image" class="n-widget w-image">
+            <img src="assets/b51e2aca93f121d72ada4fc5db77fe93.jpg" />
+          </div>
+          <div id="w-area-aaak" data-type="area" class="p-166372 n-widget w-area">
+            <div id="w-columnbox-aaal" data-type="columnbox" class="n-widget w-columnbox">
+                <div class="w-col" style="width:33.333333333333%;">
+                <div id="w-area-aaam" data-type="area" class="p-166372 n-widget w-area">
+                  <div id="w-image-text-aaan" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aaao" data-type="image" class="n-widget w-image">
+                      <img src="assets/80cd9564200497512cfdc66a06b1b818.png" />
+                    </div>
+                    <div id="w-text-aaap" data-type="text" class="n-widget w-text">
+                      <font size="3">大理石家具之鉴别须知</font><br><font color="#818181">市场上出售的大理石家具有天然大理石和人造大理石之分，而天然大理石又有优质大理石和劣质大理石之分</font><br>
+                    </div>
+                  </div>
+                  <div id="w-image-text-aaaq" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aaar" data-type="image" class="n-widget w-image">
+                      <img src="assets/f79d6ecab81cf8e8ef34650ba8e23e53.png" />
+                    </div>
+                    <div id="w-text-aaas" data-type="text" class="n-widget w-text">
+                      <font size="3">施工和验收技巧</font><br><font color="#818181">大理石施工工艺：铺设大理石饰面板时，应彻底清除基层灰渣和杂物，用水冲刷干净、晒干。</font><br>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                <div class="w-col" style="width:33.333333333333%;">
+                <div id="w-area-aaat" data-type="area" class="p-166372 n-widget w-area">
+                  <div id="w-image-text-aaau" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aaav" data-type="image" class="n-widget w-image">
+                      <img src="assets/ec8e1923edcb543a76cec8efcfb5da90.png" />
+                    </div>
+                    <div id="w-text-aaaw" data-type="text" class="n-widget w-text">
+                      <font size="3">教你选择大理石</font><br><font color="#818181">大理石装饰材料有自然大理石和人工大理石两类。自然大理石质地坚硬、颜色变化多端、深浅不一、有多种光泽</font><br>
+                    </div>
+                  </div>
+                  <div id="w-image-text-aaax" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aaay" data-type="image" class="n-widget w-image">
+                      <img src="assets/f3099fb2253e141be881d498db1205fc.png" />
+                    </div>
+                    <div id="w-text-aaaz" data-type="text" class="n-widget w-text">
+                      <font size="3">花岗岩染红板制作流程</font><br><font color="#818181">花岗石是一种由火山爆发的熔岩在受到相当的压力的熔融状态下隆起至花岗石地壳表层，岩浆不喷出地面，而在地底下慢慢冷却</font><br>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                <div class="w-col" style="width:33.333333333333%;">
+                <div id="w-area-aaba" data-type="area" class="p-166372 n-widget w-area">
+                  <div id="w-image-text-aabb" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aabc" data-type="image" class="n-widget w-image">
+                      <img src="assets/fee50a4e5bdef7fc6669cf718081c9f1.png" />
+                    </div>
+                    <div id="w-text-aabd" data-type="text" class="n-widget w-text">
+                      <font size="3">教你四招鉴别大理石的质量差异</font><br><font color="#818181" size="2">目前市场上石材产品质量良莠不齐，对于加工好的成品饰面石材，其质量好坏可以从四个方面来鉴别</font><br>
+                    </div>
+                  </div>
+                  <div id="w-image-text-aabe" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                    <div id="w-image-aabf" data-type="image" class="n-widget w-image">
+                      <img src="assets/72069dfb6886963884eb3c9769ac651c.png" />
+                    </div>
+                    <div id="w-text-aabg" data-type="text" class="n-widget w-text">
+                      <font size="3">生产制作过程</font><br><font color="#818181">色液浓度：不同浓度得到不同的色效果，生产中应看实际情况而定，一般浓度为20%为最好。</font><br>
+                    </div>
+                  </div>
+                </div>
+                </div>
+            </div>
+          </div>
+          <div id="w-divider-aabh" data-type="divider" class="n-widget w-divider">
+            <hr />
+          </div>
+          <div id="w-titlebox-aabi" data-type="titlebox" class="n-widget w-titlebox t-titlebox-00">
+            <div id="w-titlebox-aabi-title" class="w-titlebox-title t-titlebox-00-title">
+              <i class="middle"></i>
+              <h2><i></i>产品中心</h2>
+            </div>
+            <div id="w-area-aabj" data-type="area" class="p-166372 n-widget w-area w-titlebox-content t-titlebox-00-content">
+              <div id="w-columnbox-aabk" data-type="columnbox" class="n-widget w-columnbox">
+                  <div class="w-col" style="width:25%;">
+                  <div id="w-area-aabl" data-type="area" class="p-166372 n-widget w-area">
+                    <div id="w-image-text-aabm" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                      <div id="w-image-aabn" data-type="image" class="n-widget w-image">
+                        <img src="assets/8fde5f8dc67da0e3fb9ae8232240d95b.jpg" />
+                        <p class="caption">人造板</p>
+                      </div>
+                      <div id="w-text-aabo" data-type="text" class="n-widget w-text">
+                        <div align="left"><font color="#818181">产品具有无毒性、无放射性、阻燃性、不粘油、不渗污、抗菌防霉</font></div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div class="w-col" style="width:25%;">
+                  <div id="w-area-aabp" data-type="area" class="p-166372 n-widget w-area">
+                    <div id="w-image-text-aabq" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                      <div id="w-image-aabr" data-type="image" class="n-widget w-image">
+                        <img src="assets/e8865e71fd76922ba12ff5f6e3dee857.jpg" />
+                        <p class="caption">秋之叶</p>
+                      </div>
+                      <div id="w-text-aabs" data-type="text" class="n-widget w-text">
+                        <div align="left"><font color="#818181">板岩是具有板状结构，基本没有重结晶的岩石，是一种变质岩</font></div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div class="w-col" style="width:25%;">
+                  <div id="w-area-aabt" data-type="area" class="p-166372 n-widget w-area">
+                    <div id="w-image-text-aabu" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                      <div id="w-image-aabv" data-type="image" class="n-widget w-image">
+                        <img src="assets/92d4d0d905c06c86ef24bd8593daa2c2.jpg" />
+                        <p class="caption">翡翠绿</p>
+                      </div>
+                      <div id="w-text-aabw" data-type="text" class="n-widget w-text">
+                        <div align="left"><font color="#818181">板岩是具有板状结构，基本没有重结晶的岩石，是一种变质岩</font></div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div class="w-col" style="width:25%;">
+                  <div id="w-area-aabx" data-type="area" class="p-166372 n-widget w-area">
+                    <div id="w-image-text-aaby" data-type="image-text" class="n-widget w-image-text t-image-text-default">
+                      <div id="w-image-aabz" data-type="image" class="n-widget w-image">
+                        <img src="assets/44df1647f608f7ec1b357554d6f5176a.jpg" />
+                        <p class="caption">大理黑</p>
+                      </div>
+                      <div id="w-text-aaca" data-type="text" class="n-widget w-text">
+                        <div align="left"><font color="#818181">大理石主要用于加工成各种形材、板材，作建筑物的墙面</font></div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="w-area-aacb" data-type="area" class="p-166372 n-widget w-area">
+      <div id="w-text-aacc" data-type="text" class="n-widget w-text">
+        <div align="center">Power By <a href="http://www.9466.com">9466</a><br></div>
+      </div>
+    </div>
+  <!--custom footer start-->
+<script type="text/javascript">var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fce299e0fd4094e4d5efce40bab839811' type='text/javascript'%3E%3C/script%3E"));</script>
+<!--custom footer end-->
+  </body>
+</html>
